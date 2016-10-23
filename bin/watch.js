6b109ddf6e2c800c5d6cf5b9ffc8f46192725e98
Moritz Kröger <write@morkro.de>
2016-10-23 15:04:48 +0200

    Updated output

--- bin/watch.js
+++ bin/watch.js
@@ -5,31 +5,36 @@ const createJS = require('./build-js')
 const watch = require('node-watch')
 const debug = require('debug')('tetrys:watcher')
 
+function log (file, txt) {
+	debug(`${file} changed`)
+	debug(txt)
+}
+
 module.exports = () => {
 	debug('start watching files')
 
-	watch('./src/markup', () => {
-		debug('rebuild html')
+	watch('./src/markup', (file) => {
+		log(file, 'rebuild html')
 		createHTML()
 	})
 
-	watch('./src/meta', () => {
-		debug('rebuild assets')
+	watch('./src/meta', (file) => {
+		log(file, 'rebuild assets')
 		moveAssets()
 	})
 
-	watch('./src/scripts', () => {
-		debug('rebuild scripts')
+	watch('./src/scripts', (file) => {
+		log(file, 'rebuild scripts')
 		Promise.all([createJS.main(), createJS.worker()])
 	})
 
-	watch('./package.json', () => {
-		debug('rebuild service worker')
+	watch('./package.json', (file) => {
+		log(file, 'rebuild service worker')
 		createJS.worker()
 	})
 
-	watch('./src/styles', () => {
-		debug('rebuild css')
+	watch('./src/styles', (file) => {
+		log(file, 'rebuild css')
 		createCSS()
 	})
 }
